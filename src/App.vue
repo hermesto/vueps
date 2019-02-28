eef8d907cd855d048fe91e8904bdbecdd3b7a98a
Ernesto <ernesto.gmz.zcrs@gmail.com>
2019-02-28 17:52:46 -0600

    error eslint :(

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  #app
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(type="text", placeholder="buscar canciones",v-model="searchQuery")
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
+
+      .container.results
+        .columns
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
+</template>
+
+<script>
+const tracks = [
+  { name: 'Muchacha', artist: 'Luis Alberto Spinneta' },
+  { name: 'Hoy aca en el baile', artist: 'H' },
+  { name: 'La chachalaca del otro piso', artist: 'anonimo XD' }
+]
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `Encontrados ${this.tracks.length}`
+    }
+  },
+  methods: {
+    search () {
+      this.tracks = tracks
+      console.log(this.searchQuery)
+    }
+  }
+
+}
+
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss';
+  .results {
+    margin-top: 50px
+  }
+</style>
